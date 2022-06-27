f0e87aa2aa437e44f27e117df113bc119d06de6b
ArpanRamani <ramnaiarpan9@gmail.com>
2022-06-27 16:59:17 +0530

    instagramui

---
+++ app/src/main/java/com/example/instagramui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.instagramui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    }
